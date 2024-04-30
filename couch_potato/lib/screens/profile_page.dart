4d037c7ecbcad0b4aa77620a96fc9da1ec893da8
charchalis <charchalis97@gmail.com>
2024-04-30 22:34:37 +0100

    final refactor

--- couch_potato/lib/screens/profile_page.dart
+++ couch_potato/lib/screens/profile_page.dart
@@ -10,7 +10,8 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container();
+    /*return Center(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -45,6 +46,7 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
     );
+    */
   }
 }
 
