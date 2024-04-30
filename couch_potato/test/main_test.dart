9dba336e0e0ed5542c69c04ebc6bb45c2ed17bd5
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-04-30 20:31:16 +0100

    start uni tests

---
+++ couch_potato/test/main_test.dart
@@ -0,0 +1,51 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:couch_potato/main.dart';
+
+void main() async {
+  /*
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+
+  testWidgets('MyApp should build without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+
+  testWidgets('MyApp title should match', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('COUCH POTATO'), findsOneWidget);
+  });
+
+  testWidgets('MyHome should build without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHome()));
+    expect(find.byType(Scaffold), findsOneWidget);
+  });
+
+  testWidgets('MyHome should have an AppBar', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHome()));
+    expect(find.byType(AppBar), findsOneWidget);
+  });
+
+  testWidgets('MyHome should have a PageView', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHome()));
+    expect(find.byType(PageView), findsOneWidget);
+  });
+
+  testWidgets('MyHome should have a bottomNavigationBar', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHome()));
+    expect(find.byType(BottomNavigationBar), findsOneWidget);
+  });
+*/
+
+}
+
