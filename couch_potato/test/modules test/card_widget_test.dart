0a93bb96d95baad2f6196ccec7b38075228b0207
charchalis <charchalis97@gmail.com>
2024-05-13 17:18:42 +0100

    got 99 problems but git aint one

--- couch_potato/test/modules test/card_widget_test.dart
+++ couch_potato/test/modules test/card_widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:couch_potato/modules/card_widget.dart';
-import 'package:tuple/tuple.dart';
 
 void main() {
   group('CardWidget', () {
